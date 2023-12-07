7e2a51cb73c24faecb1c4b0a74d730c2c1ad433a
Josue <jaccobjosue@gmail.com>
2023-12-06 23:19:18 -0600

    versionk

--- TorresDeLeyva/Form1.cs
+++ TorresDeLeyva/Form1.cs
@@ -28,7 +28,7 @@ namespace TorresDeLeyva
         {
             MessageBox.Show(""+texto);
         }
-        private string MensajeArray(int[,] arr)
+        private string MensajeMatriz(int[,] arr)
         {
             string texto = "";
             for (int fila = 0; fila < registro.GetLength(0); fila++)
@@ -39,6 +39,16 @@ namespace TorresDeLeyva
                 }
                 texto += "\n"; 
             }
+            return texto;
+        }
+        private string MensajeArreglo(int[] arr)
+        {
+            string texto = "";
+            for (int i = 0; i < arr.Length; i++)
+            {
+                texto += (arr[i] + "\t");
+            }
+            texto += "\n";
             return texto;
         }
         private void centrarPanelInternoHorizontal(Panel panelInterno, Panel panelExterno)
@@ -95,62 +105,355 @@ namespace TorresDeLeyva
             };
 
             Random random = new Random();
-
-            // Asegurar que haya al menos un 1, un 2 y un 3 por fila
-            for (int grupo = 0; grupo < 3; grupo++)
+            int cont = 0;
+            int vuelta = 0;
+            while(cont < 9 && vuelta < 1000)
             {
-                // Obtener las filas correspondientes al grupo
-                int filaInicio = grupo * 3;
-                int filaFin = filaInicio + 2;
-
-                // Desordenar la lista de números específicos
-                int[] numeros = { 1, 2, 3 };
-                Shuffle(numeros, random);
-
-                // Asignar un número específico a cada columna en el grupo actual
-                for (int i = 0; i < 3; i++)
+                vuelta++;
+                // Obtener un número entre 0 y 3
+                int numeroColumna = random.Next(0, 4);
+                // Obtener un número entre 1 y 3
+                int numeroCategoria = random.Next(1, 4);
+                int[] arrray = ObtenerElementosColumna(numeroColumna-1);
+                if (!arrray.Contains(1))
                 {
-                    int columna;
-                    do
+                    switch (numeroCategoria)
                     {
-                        // Desordenar la lista de columnas
-                        List<int> columnasDisponibles = Enumerable.Range(0, 4).ToList();
-                        Shuffle(columnasDisponibles, random);
+                        case 1:
+                            if (!TieneValorEnFila(0))
+                            {   
+                                int[] fila1 = ObtenerFila(0);
+                                if (fila1.Contains(1))
+                                {
+                                    registro[0, numeroColumna] = 1;
+                                    cont++;
+                                }
+                                
+                                
+                            }else if (!TieneValorEnFila(1))
+                            {
+                                int[] fila2 = ObtenerFila(1);
+                                if (fila2.Contains(1))
+                                {
+                                    registro[1, numeroColumna] = 1;
+                                    cont++;
+                                }
+                            }
+                            else if (!TieneValorEnFila(2))
+                            {
+                                int[] fila3 = ObtenerFila(2);
+                                if (fila3.Contains(1))
+                                {
+                                    registro[2, numeroColumna] = 1;
+                                    cont++;
+                                }
+                            }
+                                break;
+                        case 2:
+                            if (!TieneValorEnFila(0+3))
+                            {
+                                int[] fila = ObtenerFila(0+3);
+                                if (fila.Contains(1))
+                                {
+                                    registro[0+3, numeroColumna] = 1;
+                                    cont++;
+                                }
 
-                        // Obtener la primera columna disponible
-                        columna = columnasDisponibles[0];
 
-                        // Verificar si la columna ya contiene el número
-                    } while (registro[filaInicio + i, columna] != 0);
+                            }
+                            else if (!TieneValorEnFila(1+3))
+                            {
+                                int[] fila = ObtenerFila(1+3);
+                                if (fila.Contains(1))
+                                {
+                                    registro[1+3, numeroColumna] = 1;
+                                    cont++;
+                                }
+                            }
+                            else if (!TieneValorEnFila(2+3))
+                            {
+                                int[] fila = ObtenerFila(2+3);
+                                if (fila.Contains(1))
+                                {
+                                    registro[2+3, numeroColumna] = 1;
+                                    cont++;
+                                }
+                            }
+                            break;
+                        case 3:
+                            if (!TieneValorEnFila(0 + 6))
+                            {
+                                int[] fila = ObtenerFila(0 + 6);
+                                if (fila.Contains(1))
+                                {
+                                    registro[0 + 6, numeroColumna] = 1;
+                                    cont++;
+                                }
 
-                    // Asignar número a columna en la fila
-                    registro[filaInicio + i, columna] = numeros[i];
+
+                            }
+                            else if (!TieneValorEnFila(1 + 6))
+                            {
+                                int[] fila = ObtenerFila(1 + 6);
+                                if (fila.Contains(1))
+                                {
+                                    registro[1 + 6, numeroColumna] = 1;
+                                    cont++;
+                                }
+                            }
+                            else if (!TieneValorEnFila(2 + 6))
+                            {
+                                int[] fila = ObtenerFila(2 + 6);
+                                if (fila.Contains(1))
+                                {
+                                    registro[2 + 6, numeroColumna] = 1;
+                                    cont++;
+                                }
+                            }
+                            break;
+                        default:
+                            break;
+                    }
+                }else if (!arrray.Contains(2))
+                {
+                    switch (numeroCategoria)
+                    {
+                        case 1:
+                            if (!TieneValorEnFila(0))
+                            {
+                                int[] fila1 = ObtenerFila(0);
+                                if (fila1.Contains(1))
+                                {
+                                    registro[0, numeroColumna] = 2;
+                                    cont++;
+                                }
+
+
+                            }
+                            else if (!TieneValorEnFila(1))
+                            {
+                                int[] fila2 = ObtenerFila(1);
+                                if (fila2.Contains(1))
+                                {
+                                    registro[1, numeroColumna] = 2;
+                                    cont++;
+                                }
+                            }
+                            else if (!TieneValorEnFila(2))
+                            {
+                                int[] fila3 = ObtenerFila(2);
+                                if (fila3.Contains(1))
+                                {
+                                    registro[2, numeroColumna] = 2;
+                                    cont++;
+                                }
+                            }
+                            break;
+                        case 2:
+                            if (!TieneValorEnFila(0 + 3))
+                            {
+                                int[] fila = ObtenerFila(0 + 3);
+                                if (fila.Contains(1))
+                                {
+                                    registro[0 + 3, numeroColumna] = 2;
+                                    cont++;
+                                }
+
+
+                            }
+                            else if (!TieneValorEnFila(1 + 3))
+                            {
+                                int[] fila = ObtenerFila(1 + 3);
+                                if (fila.Contains(1))
+                                {
+                                    registro[1 + 3, numeroColumna] = 2;
+                                    cont++;
+                                }
+                            }
+                            else if (!TieneValorEnFila(2 + 3))
+                            {
+                                int[] fila = ObtenerFila(2 + 3);
+                                if (fila.Contains(1))
+                                {
+                                    registro[2 + 3, numeroColumna] = 2;
+                                    cont++;
+                                }
+                            }
+                            break;
+                        case 3:
+                            if (!TieneValorEnFila(0 + 6))
+                            {
+                                int[] fila = ObtenerFila(0 + 6);
+                                if (fila.Contains(1))
+                                {
+                                    registro[0 + 6, numeroColumna] = 2;
+                                    cont++;
+                                }
+
+
+                            }
+                            else if (!TieneValorEnFila(1 + 6))
+                            {
+                                int[] fila = ObtenerFila(1 + 6);
+                                if (fila.Contains(1))
+                                {
+                                    registro[1 + 6, numeroColumna] = 2;
+                                    cont++;
+                                }
+                            }
+                            else if (!TieneValorEnFila(2 + 6))
+                            {
+                                int[] fila = ObtenerFila(2 + 6);
+                                if (fila.Contains(1))
+                                {
+                                    registro[2 + 6, numeroColumna] = 2;
+                                    cont++;
+                                }
+                            }
+                            break;
+                        default:
+                            break;
+                    }
+                }
+                else if (!arrray.Contains(3))
+                {
+                    switch (numeroCategoria)
+                    {
+                        case 1:
+                            if (!TieneValorEnFila(0))
+                            {
+                                int[] fila1 = ObtenerFila(0);
+                                if (fila1.Contains(1))
+                                {
+                                    registro[0, numeroColumna] = 3;
+                                    cont++;
+                                }
+
+
+                            }
+                            else if (!TieneValorEnFila(1))
+                            {
+                                int[] fila2 = ObtenerFila(1);
+                                if (fila2.Contains(1))
+                                {
+                                    registro[1, numeroColumna] = 3;
+                                    cont++;
+                                }
+                            }
+                            else if (!TieneValorEnFila(2))
+                            {
+                                int[] fila3 = ObtenerFila(2);
+                                if (fila3.Contains(1))
+                                {
+                                    registro[2, numeroColumna] = 3;
+                                    cont++;
+                                }
+                            }
+                            break;
+                        case 2:
+                            if (!TieneValorEnFila(0 + 3))
+                            {
+                                int[] fila = ObtenerFila(0 + 3);
+                                if (fila.Contains(1))
+                                {
+                                    registro[0 + 3, numeroColumna] = 3;
+                                    cont++;
+                                }
+
+
+                            }
+                            else if (!TieneValorEnFila(1 + 3))
+                            {
+                                int[] fila = ObtenerFila(1 + 3);
+                                if (fila.Contains(1))
+                                {
+                                    registro[1 + 3, numeroColumna] = 3;
+                                    cont++;
+                                }
+                            }
+                            else if (!TieneValorEnFila(2 + 3))
+                            {
+                                int[] fila = ObtenerFila(2 + 3);
+                                if (fila.Contains(1))
+                                {
+                                    registro[2 + 3, numeroColumna] = 3;
+                                    cont++;
+                                }
+                            }
+                            break;
+                        case 3:
+                            if (!TieneValorEnFila(0 + 6))
+                            {
+                                int[] fila = ObtenerFila(0 + 6);
+                                if (fila.Contains(1))
+                                {
+                                    registro[0 + 6, numeroColumna] = 3;
+                                    cont++;
+                                }
+
+
+                            }
+                            else if (!TieneValorEnFila(1 + 6))
+                            {
+                                int[] fila = ObtenerFila(1 + 6);
+                                if (fila.Contains(1))
+                                {
+                                    registro[1 + 6, numeroColumna] = 3;
+                                    cont++;
+                                }
+                            }
+                            else if (!TieneValorEnFila(2 + 6))
+                            {
+                                int[] fila = ObtenerFila(2 + 6);
+                                if (fila.Contains(1))
+                                {
+                                    registro[2 + 6, numeroColumna] = 3;
+                                    cont++;
+                                }
+                            }
+                            break;
+                        default:
+                            break;
+                    }
                 }
             }
-            Mensaje("Tabla: \n" + MensajeArray(registro));
+
+            Mensaje("Tabla: \n" + MensajeMatriz(registro));
             
         }
-        static void Shuffle<T>(T[] array, Random random)
+        int[] ObtenerElementosColumna(int indiceColumna)
         {
-            int n = array.Length;
-            for (int i = n - 1; i > 0; i--)
+            int[] elementosColumna = new int[9];
+
+            for (int fila = 0; fila < 9; fila++)
             {
-                int j = random.Next(0, i + 1);
-                T temp = array[i];
-                array[i] = array[j];
-                array[j] = temp;
+                elementosColumna[fila] = registro[fila, indiceColumna];
             }
+
+            return elementosColumna;
         }
-        static void Shuffle(List<int> list, Random random)
+        bool TieneValorEnFila( int fila)
         {
-            int n = list.Count;
-            for (int i = n - 1; i > 0; i--)
+            for (int columna = 0; columna < registro.GetLength(1); columna++)
             {
-                int j = random.Next(0, i + 1);
-                int temp = list[i];
-                list[i] = list[j];
-                list[j] = temp;
+                if (registro[fila, columna] != 0)
+                {
+                    return true;
+                }
             }
+            return false;
         }
+        int[] ObtenerFila(int indiceFila)
+        {
+            int[] fila = new int[registro.GetLength(1)];
+
+            for (int i = 0; i < registro.GetLength(1); i++)
+            {
+                fila[i] = registro[indiceFila, i];
+            }
+
+            return fila;
+        }
+       
     }
 }
