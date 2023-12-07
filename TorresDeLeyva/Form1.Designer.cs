324c6ad7af32b08577140b48e015719649d27aa5
Josue <jaccobjosue@gmail.com>
2023-12-06 20:28:23 -0600

    paso1

--- TorresDeLeyva/Form1.Designer.cs
+++ TorresDeLeyva/Form1.Designer.cs
@@ -29,7 +29,242 @@ namespace TorresDeLeyva
         /// </summary>
         private void InitializeComponent()
         {
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.pnContenedorJg1 = new System.Windows.Forms.Panel();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.pnContenedorJg2 = new System.Windows.Forms.Panel();
+            this.panel4 = new System.Windows.Forms.Panel();
+            this.pnContenedorJg3 = new System.Windows.Forms.Panel();
+            this.panel6 = new System.Windows.Forms.Panel();
+            this.pnContenedorJg4 = new System.Windows.Forms.Panel();
+            this.panel8 = new System.Windows.Forms.Panel();
+            this.pnBast1Lv1 = new System.Windows.Forms.Panel();
+            this.pnBast1Lv2 = new System.Windows.Forms.Panel();
+            this.pnBast1Lv3 = new System.Windows.Forms.Panel();
+            this.pnBast2Lv3 = new System.Windows.Forms.Panel();
+            this.pnBast2Lv2 = new System.Windows.Forms.Panel();
+            this.pnBast2Lv1 = new System.Windows.Forms.Panel();
+            this.pnBast3Lv1 = new System.Windows.Forms.Panel();
+            this.pnBast3Lv2 = new System.Windows.Forms.Panel();
+            this.pnBast3Lv3 = new System.Windows.Forms.Panel();
+            this.pnBast4Lv1 = new System.Windows.Forms.Panel();
+            this.pnBast4Lv2 = new System.Windows.Forms.Panel();
+            this.pnBast4Lv3 = new System.Windows.Forms.Panel();
+            this.btnSalir = new System.Windows.Forms.Button();
+            this.btnIniciar = new System.Windows.Forms.Button();
+            this.groupBox1.SuspendLayout();
+            this.pnContenedorJg1.SuspendLayout();
+            this.pnContenedorJg2.SuspendLayout();
+            this.pnContenedorJg3.SuspendLayout();
+            this.pnContenedorJg4.SuspendLayout();
             this.SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.pnContenedorJg4);
+            this.groupBox1.Controls.Add(this.pnContenedorJg3);
+            this.groupBox1.Controls.Add(this.pnContenedorJg2);
+            this.groupBox1.Controls.Add(this.pnContenedorJg1);
+            this.groupBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.groupBox1.Location = new System.Drawing.Point(12, 12);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(563, 388);
+            this.groupBox1.TabIndex = 0;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Jugador";
+            // 
+            // pnContenedorJg1
+            // 
+            this.pnContenedorJg1.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+            this.pnContenedorJg1.Controls.Add(this.pnBast1Lv3);
+            this.pnContenedorJg1.Controls.Add(this.pnBast1Lv2);
+            this.pnContenedorJg1.Controls.Add(this.pnBast1Lv1);
+            this.pnContenedorJg1.Controls.Add(this.panel2);
+            this.pnContenedorJg1.Location = new System.Drawing.Point(22, 73);
+            this.pnContenedorJg1.Name = "pnContenedorJg1";
+            this.pnContenedorJg1.Size = new System.Drawing.Size(114, 309);
+            this.pnContenedorJg1.TabIndex = 0;
+            // 
+            // panel2
+            // 
+            this.panel2.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(157)))), ((int)(((byte)(104)))), ((int)(((byte)(15)))));
+            this.panel2.Location = new System.Drawing.Point(35, 32);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(43, 274);
+            this.panel2.TabIndex = 0;
+            // 
+            // pnContenedorJg2
+            // 
+            this.pnContenedorJg2.Controls.Add(this.pnBast2Lv1);
+            this.pnContenedorJg2.Controls.Add(this.pnBast2Lv2);
+            this.pnContenedorJg2.Controls.Add(this.pnBast2Lv3);
+            this.pnContenedorJg2.Controls.Add(this.panel4);
+            this.pnContenedorJg2.Location = new System.Drawing.Point(151, 73);
+            this.pnContenedorJg2.Name = "pnContenedorJg2";
+            this.pnContenedorJg2.Size = new System.Drawing.Size(114, 309);
+            this.pnContenedorJg2.TabIndex = 1;
+            // 
+            // panel4
+            // 
+            this.panel4.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(157)))), ((int)(((byte)(104)))), ((int)(((byte)(15)))));
+            this.panel4.Location = new System.Drawing.Point(37, 32);
+            this.panel4.Name = "panel4";
+            this.panel4.Size = new System.Drawing.Size(43, 274);
+            this.panel4.TabIndex = 0;
+            // 
+            // pnContenedorJg3
+            // 
+            this.pnContenedorJg3.Controls.Add(this.pnBast3Lv3);
+            this.pnContenedorJg3.Controls.Add(this.pnBast3Lv2);
+            this.pnContenedorJg3.Controls.Add(this.pnBast3Lv1);
+            this.pnContenedorJg3.Controls.Add(this.panel6);
+            this.pnContenedorJg3.Location = new System.Drawing.Point(280, 73);
+            this.pnContenedorJg3.Name = "pnContenedorJg3";
+            this.pnContenedorJg3.Size = new System.Drawing.Size(114, 309);
+            this.pnContenedorJg3.TabIndex = 2;
+            // 
+            // panel6
+            // 
+            this.panel6.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(157)))), ((int)(((byte)(104)))), ((int)(((byte)(15)))));
+            this.panel6.Location = new System.Drawing.Point(38, 32);
+            this.panel6.Name = "panel6";
+            this.panel6.Size = new System.Drawing.Size(43, 274);
+            this.panel6.TabIndex = 0;
+            // 
+            // pnContenedorJg4
+            // 
+            this.pnContenedorJg4.Controls.Add(this.pnBast4Lv3);
+            this.pnContenedorJg4.Controls.Add(this.pnBast4Lv2);
+            this.pnContenedorJg4.Controls.Add(this.pnBast4Lv1);
+            this.pnContenedorJg4.Controls.Add(this.panel8);
+            this.pnContenedorJg4.Location = new System.Drawing.Point(409, 73);
+            this.pnContenedorJg4.Name = "pnContenedorJg4";
+            this.pnContenedorJg4.Size = new System.Drawing.Size(114, 309);
+            this.pnContenedorJg4.TabIndex = 3;
+            // 
+            // panel8
+            // 
+            this.panel8.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(157)))), ((int)(((byte)(104)))), ((int)(((byte)(15)))));
+            this.panel8.Location = new System.Drawing.Point(37, 32);
+            this.panel8.Name = "panel8";
+            this.panel8.Size = new System.Drawing.Size(43, 274);
+            this.panel8.TabIndex = 0;
+            // 
+            // pnBast1Lv1
+            // 
+            this.pnBast1Lv1.BackColor = System.Drawing.Color.Yellow;
+            this.pnBast1Lv1.Location = new System.Drawing.Point(26, 59);
+            this.pnBast1Lv1.Name = "pnBast1Lv1";
+            this.pnBast1Lv1.Size = new System.Drawing.Size(61, 50);
+            this.pnBast1Lv1.TabIndex = 1;
+            // 
+            // pnBast1Lv2
+            // 
+            this.pnBast1Lv2.BackColor = System.Drawing.Color.Yellow;
+            this.pnBast1Lv2.Location = new System.Drawing.Point(26, 132);
+            this.pnBast1Lv2.Name = "pnBast1Lv2";
+            this.pnBast1Lv2.Size = new System.Drawing.Size(61, 50);
+            this.pnBast1Lv2.TabIndex = 2;
+            // 
+            // pnBast1Lv3
+            // 
+            this.pnBast1Lv3.BackColor = System.Drawing.Color.Yellow;
+            this.pnBast1Lv3.Location = new System.Drawing.Point(26, 205);
+            this.pnBast1Lv3.Name = "pnBast1Lv3";
+            this.pnBast1Lv3.Size = new System.Drawing.Size(61, 50);
+            this.pnBast1Lv3.TabIndex = 3;
+            // 
+            // pnBast2Lv3
+            // 
+            this.pnBast2Lv3.BackColor = System.Drawing.Color.Yellow;
+            this.pnBast2Lv3.Location = new System.Drawing.Point(28, 205);
+            this.pnBast2Lv3.Name = "pnBast2Lv3";
+            this.pnBast2Lv3.Size = new System.Drawing.Size(61, 50);
+            this.pnBast2Lv3.TabIndex = 4;
+            // 
+            // pnBast2Lv2
+            // 
+            this.pnBast2Lv2.BackColor = System.Drawing.Color.Yellow;
+            this.pnBast2Lv2.Location = new System.Drawing.Point(28, 132);
+            this.pnBast2Lv2.Name = "pnBast2Lv2";
+            this.pnBast2Lv2.Size = new System.Drawing.Size(61, 50);
+            this.pnBast2Lv2.TabIndex = 5;
+            // 
+            // pnBast2Lv1
+            // 
+            this.pnBast2Lv1.BackColor = System.Drawing.Color.Yellow;
+            this.pnBast2Lv1.Location = new System.Drawing.Point(28, 59);
+            this.pnBast2Lv1.Name = "pnBast2Lv1";
+            this.pnBast2Lv1.Size = new System.Drawing.Size(61, 50);
+            this.pnBast2Lv1.TabIndex = 6;
+            // 
+            // pnBast3Lv1
+            // 
+            this.pnBast3Lv1.BackColor = System.Drawing.Color.Yellow;
+            this.pnBast3Lv1.Location = new System.Drawing.Point(29, 59);
+            this.pnBast3Lv1.Name = "pnBast3Lv1";
+            this.pnBast3Lv1.Size = new System.Drawing.Size(61, 50);
+            this.pnBast3Lv1.TabIndex = 7;
+            // 
+            // pnBast3Lv2
+            // 
+            this.pnBast3Lv2.BackColor = System.Drawing.Color.Yellow;
+            this.pnBast3Lv2.Location = new System.Drawing.Point(29, 132);
+            this.pnBast3Lv2.Name = "pnBast3Lv2";
+            this.pnBast3Lv2.Size = new System.Drawing.Size(61, 50);
+            this.pnBast3Lv2.TabIndex = 8;
+            // 
+            // pnBast3Lv3
+            // 
+            this.pnBast3Lv3.BackColor = System.Drawing.Color.Yellow;
+            this.pnBast3Lv3.Location = new System.Drawing.Point(29, 205);
+            this.pnBast3Lv3.Name = "pnBast3Lv3";
+            this.pnBast3Lv3.Size = new System.Drawing.Size(61, 50);
+            this.pnBast3Lv3.TabIndex = 8;
+            // 
+            // pnBast4Lv1
+            // 
+            this.pnBast4Lv1.BackColor = System.Drawing.Color.Yellow;
+            this.pnBast4Lv1.Location = new System.Drawing.Point(28, 59);
+            this.pnBast4Lv1.Name = "pnBast4Lv1";
+            this.pnBast4Lv1.Size = new System.Drawing.Size(61, 50);
+            this.pnBast4Lv1.TabIndex = 9;
+            // 
+            // pnBast4Lv2
+            // 
+            this.pnBast4Lv2.BackColor = System.Drawing.Color.Yellow;
+            this.pnBast4Lv2.Location = new System.Drawing.Point(28, 132);
+            this.pnBast4Lv2.Name = "pnBast4Lv2";
+            this.pnBast4Lv2.Size = new System.Drawing.Size(61, 50);
+            this.pnBast4Lv2.TabIndex = 10;
+            // 
+            // pnBast4Lv3
+            // 
+            this.pnBast4Lv3.BackColor = System.Drawing.Color.Yellow;
+            this.pnBast4Lv3.Location = new System.Drawing.Point(28, 205);
+            this.pnBast4Lv3.Name = "pnBast4Lv3";
+            this.pnBast4Lv3.Size = new System.Drawing.Size(61, 50);
+            this.pnBast4Lv3.TabIndex = 11;
+            // 
+            // btnSalir
+            // 
+            this.btnSalir.Location = new System.Drawing.Point(581, 371);
+            this.btnSalir.Name = "btnSalir";
+            this.btnSalir.Size = new System.Drawing.Size(75, 23);
+            this.btnSalir.TabIndex = 1;
+            this.btnSalir.Text = "Salir";
+            this.btnSalir.UseVisualStyleBackColor = true;
+            this.btnSalir.Click += new System.EventHandler(this.btnSalir_Click);
+            // 
+            // btnIniciar
+            // 
+            this.btnIniciar.Location = new System.Drawing.Point(581, 329);
+            this.btnIniciar.Name = "btnIniciar";
+            this.btnIniciar.Size = new System.Drawing.Size(75, 23);
+            this.btnIniciar.TabIndex = 3;
+            this.btnIniciar.Text = "Iniciar";
+            this.btnIniciar.UseVisualStyleBackColor = true;
+            this.btnIniciar.Click += new System.EventHandler(this.btnIniciar_Click);
             // 
             // Form1
             // 
@@ -37,14 +272,46 @@ namespace TorresDeLeyva
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.SystemColors.HotTrack;
             this.ClientSize = new System.Drawing.Size(1252, 450);
+            this.Controls.Add(this.btnIniciar);
+            this.Controls.Add(this.btnSalir);
+            this.Controls.Add(this.groupBox1);
             this.Name = "Form1";
-            this.Text = "Form1";
+            this.Text = "Las torres de Leyva ";
             this.Load += new System.EventHandler(this.Form1_Load);
+            this.groupBox1.ResumeLayout(false);
+            this.pnContenedorJg1.ResumeLayout(false);
+            this.pnContenedorJg2.ResumeLayout(false);
+            this.pnContenedorJg3.ResumeLayout(false);
+            this.pnContenedorJg4.ResumeLayout(false);
             this.ResumeLayout(false);
 
         }
 
         #endregion
+
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Panel pnContenedorJg1;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.Panel pnContenedorJg4;
+        private System.Windows.Forms.Panel pnBast4Lv3;
+        private System.Windows.Forms.Panel pnBast4Lv2;
+        private System.Windows.Forms.Panel pnBast4Lv1;
+        private System.Windows.Forms.Panel panel8;
+        private System.Windows.Forms.Panel pnContenedorJg3;
+        private System.Windows.Forms.Panel pnBast3Lv3;
+        private System.Windows.Forms.Panel pnBast3Lv2;
+        private System.Windows.Forms.Panel pnBast3Lv1;
+        private System.Windows.Forms.Panel panel6;
+        private System.Windows.Forms.Panel pnContenedorJg2;
+        private System.Windows.Forms.Panel pnBast2Lv1;
+        private System.Windows.Forms.Panel pnBast2Lv2;
+        private System.Windows.Forms.Panel pnBast2Lv3;
+        private System.Windows.Forms.Panel panel4;
+        private System.Windows.Forms.Panel pnBast1Lv3;
+        private System.Windows.Forms.Panel pnBast1Lv2;
+        private System.Windows.Forms.Panel pnBast1Lv1;
+        private System.Windows.Forms.Button btnSalir;
+        private System.Windows.Forms.Button btnIniciar;
     }
 }
 
--- TorresDeLeyva/Form1.cs
+++ TorresDeLeyva/Form1.cs
@@ -16,11 +16,219 @@ namespace TorresDeLeyva
         {
             InitializeComponent();
         }
-
         private void Form1_Load(object sender, EventArgs e)
         {
-                
         }
 
+        private void btnSalir_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+        private void Mensaje(string texto)
+        {
+            MessageBox.Show(""+texto);
+        }
+        private string MensajeArray(int[,] arr)
+        {
+            string texto = "";
+            for (int fila = 0; fila < registro.GetLength(0); fila++)
+            {
+                for (int columna = 0; columna < registro.GetLength(1); columna++)
+                {
+                    texto += (registro[fila, columna] + "\t");
+                }
+                texto += "\n"; 
+            }
+            return texto;
+        }
+        private void centrarPanelInternoHorizontal(Panel panelInterno, Panel panelExterno)
+        {
+            // Calcular las coordenadas para centrar el panel interno horizontalmente dentro del panel externo
+            int x = (panelExterno.Width - panelInterno.Width) / 2;
+            // Establecer la nueva posición del panel interno
+            panelInterno.Location = new Point(x, panelInterno.Location.Y);
+        }
+        private void setGrande(Panel pan1, Panel pan2)
+        {
+            pan1.Size = new Size(108, pan1.Size.Height);
+            centrarPanelInternoHorizontal(pan1, pan2);
+        }
+        private void setMediano(Panel pan1, Panel pan2)
+        {
+            pan1.Size = new Size(84, pan1.Size.Height);
+            centrarPanelInternoHorizontal(pan1, pan2);  
+        }
+        private void setPeque(Panel pan1, Panel pan2)
+        {
+            pan1.Size = new Size(61, pan1.Size.Height);
+            centrarPanelInternoHorizontal(pan1, pan2);
+        }
+        Stack<int> pilaBast0 = new Stack<int>();
+        Stack<int> pilaBast1 = new Stack<int>();
+        Stack<int> pilaBast2 = new Stack<int>();
+        Stack<int> pilaBast3 = new Stack<int>();
+        int[,] registro = {
+            {0, 0, 0, 0},
+            {0, 0, 0, 0},
+            {0, 0, 0, 0},
+            {0, 0, 0, 0},
+            {0, 0, 0, 0},
+            {0, 0, 0, 0},
+            {0, 0, 0, 0},
+            {0, 0, 0, 0},
+            {0, 0, 0, 0}
+        };
+        private void btnIniciar_Click(object sender, EventArgs e)
+        {
+            btnIniciar.Text = "Reiniciar";
+            registro = new int[,]
+            {
+                {0, 0, 0, 0},
+                {0, 0, 0, 0},
+                {0, 0, 0, 0},
+                {0, 0, 0, 0},
+                {0, 0, 0, 0},
+                {0, 0, 0, 0},
+                {0, 0, 0, 0},
+                {0, 0, 0, 0},
+                {0, 0, 0, 0}
+            };
+            pilaBast0 = new Stack<int>();
+            pilaBast1 = new Stack<int>();
+            pilaBast2 = new Stack<int>();
+            pilaBast3 = new Stack<int>();
+            //setGrande(pnBast1Lv3, pnContenedorJg1);
+            //setMediano(pnBast1Lv2, pnContenedorJg1);
+            //setPeque(pnBast1Lv1, pnContenedorJg1);
+            var lista = new List<int> { 1, 2, 3, 1, 2, 3, 1, 2, 3 };
+
+            // Crear un objeto Random para utilizarlo en el desordenamiento
+            Random random = new Random();
+
+            // Desordenar la lista utilizando el método Shuffle
+            var listaDesordenada = lista.OrderBy(x => random.Next()).ToList();
+            Mensaje(string.Join(", ", listaDesordenada));
+            int contV = 0,contR = 0,contA = 0;
+
+            while (listaDesordenada.Count > 0)
+            {
+                int aleatorio = random.Next(1,5);
+                switch (aleatorio)
+                {
+                    case 1:
+                        if (pilaBast0.Count < 3)
+                        {
+                            int ultimoElemento = listaDesordenada.Last();
+                            listaDesordenada.RemoveAt(listaDesordenada.Count - 1);
+                            pilaBast0.Push(ultimoElemento);
+                            int fila;
+                            if(contV < 3)
+                            {
+                                fila = contV;
+                                contV++;
+                            }else if(contR < 3){
+                                fila = contR + 3;
+                                contR++;
+                            }
+                            else
+                            {
+                                fila = contA + 6;
+                                contA++;
+                            }
+                            int Tamaño = ultimoElemento;
+                            registro[fila, 0] = Tamaño;
+                        }
+                        break;
+                    case 2:
+                        if (pilaBast1.Count < 3)
+                        {
+                            int ultimoElemento = listaDesordenada.Last();
+                            listaDesordenada.RemoveAt(listaDesordenada.Count - 1);
+                            pilaBast1.Push(ultimoElemento);
+                            int fila;
+                            if (contV < 3)
+                            {
+                                fila = contV;
+                                contV++;
+                            }
+                            else if (contR < 3)
+                            {
+                                fila = contR + 3;
+                                contR++;
+                            }
+                            else
+                            {
+                                fila = contA + 6;
+                                contA++;
+                            }
+                            int Tamaño = ultimoElemento;
+                            registro[fila, 1] = Tamaño;
+                        }
+                        break;
+                    case 3:
+                        if (pilaBast2.Count < 3)
+                        {
+                            int ultimoElemento = listaDesordenada.Last();
+                            listaDesordenada.RemoveAt(listaDesordenada.Count - 1);
+                            pilaBast2.Push(ultimoElemento);
+                            int fila;
+                            if (contV < 3)
+                            {
+                                fila = contV;
+                                contV++;
+                            }
+                            else if (contR < 3)
+                            {
+                                fila = contR + 3;
+                                contR++;
+                            }
+                            else
+                            {
+                                fila = contA + 6;
+                                contA++;
+                            }
+                            int Tamaño = ultimoElemento;
+                            registro[fila, 2] = Tamaño;
+                        }
+                        break;
+                    case 4:
+                        if (pilaBast3.Count < 3)
+                        {
+                            int ultimoElemento = listaDesordenada.Last();
+                            listaDesordenada.RemoveAt(listaDesordenada.Count - 1);
+                            pilaBast3.Push(ultimoElemento);
+                            int fila;
+                            if (contV < 3)
+                            {
+                                fila = contV;
+                                contV++;
+                            }
+                            else if (contR < 3)
+                            {
+                                fila = contR + 3;
+                                contR++;
+                            }
+                            else
+                            {
+                                fila = contA + 6;
+                                contA++;
+                            }
+                            int y = ultimoElemento;
+                            int Tamaño = ultimoElemento;
+                            registro[fila, 3] = Tamaño;
+                        }
+                        break;
+                }
+            }
+            Mensaje("Tabla: \n" + MensajeArray(registro));
+            for (int i = 0; i < registro.GetLength(0); i++)
+            {
+
+                for (int j = 0; j < registro.GetLength(1); j++)
+                {
+
+                }
+            }
+        }
     }
 }
